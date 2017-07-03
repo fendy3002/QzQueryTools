480a12b1c771112c4e63273b7351cbad253c13f5
Fendy Heryanto <fendy3002@yahoo.com>
2017-07-04 00:40:26 +0700

    use promise

--- exec.js
+++ exec.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const uuid = require('uuid/v1');
 const JSON5 = require('JSON5');
+var Promise = require('promise');
 
 var args = process.argv.slice(2);
 var helpLine = "QzQueryTools exec.js\n" +
@@ -28,31 +29,80 @@ var exec = function(args){
     }
 };
 
+var readFile = Promise.denodeify(fs.readFile);
+var writeFile = Promise.denodeify(fs.writeFile);
+var log = Promise.denodeify(console.log);
 var prepare = function(){
-    console.log("Copying connection and query files...\n");
+    console.log("Copying query files...");
     var folder = path.join(__dirname, "server/storage/config");
     var exampleQuery = fs.createReadStream(path.join(folder, 'queries', '.script.example'));
     var actualQuery = fs.createWriteStream(path.join(folder, 'queries', 'script.example'));
 
     exampleQuery.pipe(actualQuery);
     exampleQuery.on('end', () => {
-        var exampleCon = fs.createReadStream(path.join(folder, 'connections.js.example'));
-        var actualCon = fs.createWriteStream(path.join(folder, 'connections.js'));
-        exampleCon.pipe(actualCon);
-        exampleCon.on('end', () => {
-            console.log("Copying connection and query files done...\n");
+        console.log("Copying query files done");
+    });
+
+    var saveConfig = (configObj) => {
+        return new Promise((resolve, reject) => {
+            configObj.key = uuid().replace(/-/g, "");
+            fs.writeFile(path.join(folder, "config.js"), 
+                JSON.stringify(configObj, null, 4), 
+                "utf8",
+                (err) => {
+                    if(err){ reject(err); }
+                    else{
+                        console.log("Copying config files done");
+                        resolve();
+                    }
+                });
+        });
+    };
+    var saveConnection = (configObj, connectionObj) => {
+        return new Promise((resolve, reject) => {
+            for(var i = 0; i < connectionObj.length; i++){
+
+            }
+            return fs.writeFile(path.join(folder, "connection.js"), 
+                JSON.stringify(connectionObj, null, 4), 
+                "utf8", (err) => {
+                    if(err){ reject(err); }
+                    else{ resolve(); }
+                });
+        })
+    };
+    console.log("Copying config files...");
+    var getConfigObj = new Promise((resolve, reject) => {
+        var configPath = path.join(folder, "config.js.example");
+        fs.readFile(configPath, "utf8", (err, data) => {
+            var configObj = JSON5.parse(data);
+            resolve(configObj);
+        });
+    });
+    var saveConfigObj = getConfigObj.then((configObj) => {
+        return saveConfig(configObj);
+    });
+    var getConnectionObj = new Promise((resolve, reject) => {
+        console.log("Copying connection file...");
+        var connectionPath = path.join(folder, 'connections.js.example');
+        fs.readFile(connectionPath, 'utf8', (err, data) => {
+            var connectionObj = JSON5.parse(data);
+            resolve(connectionObj);
         });
     });
 
-    console.log("Copying config files...\n");
-    var appConfig = fs.readFile(path.join(folder, "config.js.example"), "utf8", (err, data) => {
-        var configObj = JSON5.parse(data);
-        configObj.key = uuid().replace(/-/g, "");
-        fs.writeFile(path.join(folder, "config.js"), 
-            JSON.stringify(configObj, null, 4), 
-            "utf8");
-        console.log("Copying config files done...\n");
-    });
+    var saveConnectionObj = Promise.all(
+        [
+            getConfigObj,
+            saveConfigObj,
+            getConnectionObj
+        ]).then((values) => {
+            return saveConnection(values[0], values[2]);
+        }).then(() => {
+            console.log("Copying connection files done");
+        }).catch(err => {
+            console.log("err", err);
+        });
 };
 
 
---
+++ server/storage/config/connection.js
@@ -0,0 +1,20 @@
+[
+    {
+        "name": "local dev",
+        "driver": "mysql",
+        "port": "3306",
+        "host": "localhost",
+        "db": "test",
+        "username": "root",
+        "password": "password"
+    },
+    {
+        "name": "local dev 2",
+        "driver": "mysql",
+        "port": "3307",
+        "host": "localhost",
+        "db": "test",
+        "username": "root",
+        "password": "password"
+    }
+]
