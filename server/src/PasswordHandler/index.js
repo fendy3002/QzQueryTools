7255608274a62d15571e9eb91885bc4ea4324a61
Fendy Heryanto <fendy3002@yahoo.com>
2017-07-05 00:20:48 +0700

    add password handler

---
+++ server/src/PasswordHandler/index.js
@@ -0,0 +1,22 @@
+import crypto from 'crypto';
+
+var algorithm = 'aes-256-ctr';
+
+var Service = function(password){
+	return {
+		encrypt: (text) => {
+			var cipher = crypto.createCipher(algorithm,password)
+			var crypted = cipher.update(text,'utf8','hex')
+			crypted += cipher.final('hex');
+			return crypted;
+		},
+		decrypt : (text) => {
+			var decipher = crypto.createDecipher(algorithm, password)
+			var dec = decipher.update(text,'hex','utf8')
+			dec += decipher.final('utf8');
+			return dec;
+		}
+	};
+};
+
+export default Service;
---
+++ test/PasswordHandler.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var PasswordHandler = require('../server/src/PasswordHandler/index.js').default;
+
+describe('PasswordHandler', function() {
+	describe('Test encrypt decrypt', function() {
+		it('should resulting the same', function() {
+			var key = "SomeRandomKey";
+			var toEncrypt = "SomeRandomStringYouKnow";
+			var encrypted = PasswordHandler(key).encrypt(toEncrypt);
+			var decrypted = PasswordHandler(key).decrypt(encrypted);
+			assert.equal(toEncrypt, decrypted);
+		});
+	});
+});
