8b5dfb72909b8c839b4b47c2af91fc23ae4fcb0e
Fendy Heryanto <fendy3002@yahoo.com>
2017-07-06 00:34:21 +0700

    fix crypto

--- console/Crypto.js
+++ console/Crypto.js
@@ -19,49 +19,52 @@ var Service = function(root){
 	        JSON.stringify(connectionObjs, null, 4), 
 	        "utf8");
 	};
+	var encrypt = function(){
+	    console.log("Trying to encrypt connection...");
+	    var encrypt = PasswordHandler(configObj.key).encrypt;
+	    template(function(password){
+	    	return encrypt(password);
+	    });
+	    console.log("Encrypt connection done");
+	};
+	var decrypt = function(){
+	    console.log("Trying to decrypt connection...");
+	    var decrypt = PasswordHandler(configObj.key).decrypt;
+	    template(function(password){
+	    	var decrypted = decrypt(password);
+	        if(decrypted){
+	        	return decrypted;
+	        }
+	        else{
+        		return password;
+	        }
+	    });
+	    console.log("Decrypt connection done");
+	};
+	var reencrypt = function(){
+	    var folder = path.join(root, "server/storage/config");
+	    decrypt();
+	    console.log("Changing config key...");
+	    var configPath = path.join(folder, "config.js");
+	    fs.readFile(configPath, "utf8", (err, data) => {
+	        var configObj = JSON5.parse(data);
+	        configObj.key = uuid().replace(/-/g, "");
+	        fs.writeFile(path.join(folder, "config.js"), 
+	            JSON.stringify(configObj, null, 4), 
+	            "utf8",
+	            (err) => {
+	                if(err){ reject(err); }
+	                else{
+	                    console.log("Changing config key done");
+	                    encrypt();
+	                }
+	            });
+	    });
+	};
 	return {
-		encrypt: function(){
-		    console.log("Trying to encrypt connection...");
-		    var encrypt = PasswordHandler(configObj.key).encrypt;
-		    template(function(password){
-		    	return encrypt(password);
-		    });
-		    console.log("Encrypt connection done");
-		},
-		decrypt: function(){
-		    console.log("Trying to decrypt connection...");
-		    var decrypt = PasswordHandler(configObj.key).decrypt;
-		    template(function(password){
-		    	var decrypted = decrypt(password);
-		        if(decrypted){
-		        	return decrypted;
-		        }
-		        else{
-	        		return password;
-		        }
-		    });
-		    console.log("Decrypt connection done");
-		},
-		reencrypt: function(){
-		    var folder = path.join(root, "server/storage/config");
-		    decrypt();
-		    console.log("Changing config key...");
-		    var configPath = path.join(folder, "config.js");
-		    fs.readFile(configPath, "utf8", (err, data) => {
-		        var configObj = JSON5.parse(data);
-		        configObj.key = uuid().replace(/-/g, "");
-		        fs.writeFile(path.join(folder, "config.js"), 
-		            JSON.stringify(configObj, null, 4), 
-		            "utf8",
-		            (err) => {
-		                if(err){ reject(err); }
-		                else{
-		                    console.log("Changing config key done");
-		                    encrypt();
-		                }
-		            });
-		    });
-		}
+		encrypt,
+		decrypt,
+		reencrypt
 	} 
 };
 
